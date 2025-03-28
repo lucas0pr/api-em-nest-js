cbfcfb116635b1025f57a402ce96805494d83f82
lucas0pr <67844162+lucas0pr@users.noreply.github.com>
2025-03-28 20:48:46 -0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+import { ProductnestModule } from './generate/productnest/productnest.module';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // ou 'mysql', 'sqlite', etc.
+      host: 'localhost',
+      port: 5432, // ou a porta do seu banco
+      username: 'seu_usuario',
+      password: 'sua_senha',
+      database: 'nome_do_banco',
+      entities: [Product],
+      synchronize: true, // Cuidado: não use em produção
+    }),
+    ProductModule,
+    ProductnestModule,
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class AppModule {}
